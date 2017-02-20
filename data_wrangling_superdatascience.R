0479fdef907a5e49140763d502b7b62f497a37e8
Muhsin Karim <Muhsin Karim>
2017-02-20 21:55:12 +1100

    code complete

--- data_wrangling_superdatascience.R
+++ data_wrangling_superdatascience.R
@@ -1,29 +1,22 @@
-##### Crimes in Chicago - Data wrangling exmaple
+##### Crimes in Chicago - Data wrangling exmaple for SuperDataScience
 
 #### Libraries
 
+## Load libraries
 library(readr)
 suppressPackageStartupMessages(library(dplyr))
-library(stringr) # install.packages("stringr", repos=c("http://rstudio.org/_packages", "http://cran.rstudio.com"))
 suppressPackageStartupMessages(library(lubridate))
-library(ggraptR) # install.packages("ggraptR", repos=c("http://rstudio.org/_packages", "http://cran.rstudio.com"))
-  # devtools::install_github('cargomoose/raptR', force = TRUE)
+library(ggplot2)
 
 
-#### Get source data
+#### Get raw data
 
 ## Set working directory
 #! Change to your working directory here !#
-setwd("C:/Users/Muhsin Karim/Documents/Data/source_data/kaggle/crimes_in_chicago")
+setwd("C:/Users/Muhsin Karim/Documents/Data/raw_data/kaggle/crimes_in_chicago")
 
 ## Load in each file
 df1 <- read_csv("Chicago_Crimes_2001_to_2004.csv")
-#df <- df1
-
-# #! Use a subset of data when writing code !#
-# df <- df1
-# df <- df[1:1000, ]
-
 df2 <- read_csv("Chicago_Crimes_2005_to_2007.csv")
 df3 <- read_csv("Chicago_Crimes_2008_to_2011.csv")
 df4 <- read_csv("Chicago_Crimes_2012_to_2017.csv")
@@ -35,8 +28,7 @@ rm(df1, df2, df3, df4)
 
 #### Format data
 
-### Remove columns
-
+## Remove columns
 df <- df %>%
   select(-X1,
          -IUCR,
@@ -44,46 +36,19 @@ df <- df %>%
          -`Y Coordinate`,
          -Location)
 
-
-### Rename columns
-
+## Rename column
 colnames(df)[colnames(df) == "Date"] <- "Datetime"
-# colnames(df)[colnames(df) == "Case Number"] <- "Case_Number"
-# colnames(df)[colnames(df) == "Primary Type"] <- "Primary_Type"
-# colnames(df)[colnames(df) == "Location Description"] <- "Location_Description"
-# colnames(df)[colnames(df) == "Community Area"] <- "Community_Area"
-# colnames(df)[colnames(df) == "FBI Code"] <- "FBI_Code"
-# colnames(df)[colnames(df) == "Updated On"] <- "Updated_On"
 
 
 ### Change data types
 
-## Character
 df$ID <- as.character(df$ID)
 
 
+### Create datetimes
 
-## Remove duplicate rows
-#df <- unique(df)
-# dfDup <- df[duplicated(df), ]
-# x <- df[which(df$ID == 4786321), ]
-# x1 <- df[which(df$ID == 8242044), ]
-# x2 <- df[which(df$ID == 643), ]
-
-df <- df[-which(duplicated(df)), ]
-
-
-
-### Change more data types
-
-## Datetime
-#am_pm <- str_sub(df$Datetime, -2, -1) # Record AM/PM
-#df$Datetime2 <- df$Datetime
-#df$Datetime <- df$Datetime2
-df$Datetime <- as.POSIXct(df$Datetime, format="%m/%d/%Y %I:%M:%S %p")
-
-## Updated On
-df$`Updated On` <- as.POSIXct(df$`Updated On`, format="%m/%d/%Y %I:%M:%S %p")
+df$Datetime <- as.POSIXct(df$Datetime, format="%m/%d/%Y %I:%M:%S %p") # Datetime
+df$`Updated On` <- as.POSIXct(df$`Updated On`, format="%m/%d/%Y %I:%M:%S %p") # Updated On
 
 
 #### Create columns
@@ -95,32 +60,26 @@ df$Month <- NA
 df$Month <- months(df$Datetime)
 
 ## Month
-df$Month_Value <- NA
-df$Month_Value <- month(df$Datetime)
+df$`Month Value` <- NA
+df$`Month Value` <- month(df$Datetime)
 
-## Year_Month
-df$Year_Month <- NA
-df$Year_Month <- paste(df$Year, sprintf("%02d", df$Month_Value), sep = "-")
+## Year Month
+df$`Year Month` <- NA
+df$`Year Month` <- paste(df$Year, sprintf("%02d", df$`Month Value`), sep = "-")
 
 ## Hour
 df$Hour <- NA
 df$Hour <- hour(df$Datetime)
 
-# # Apply 24 time if PM
-# df$Hour[which(am_pm == "PM")] <- df$Hour[which(am_pm == "PM")] + 12
-# df$Hour[which(df$Hour == 24)] <- 0 # Set midnight to 1 am to 0
-# df$Hour <- sprintf("%02d", df$Hour)
-
 
 #### Create unique identifier
-# Use ID and Case Number
 
 ## Create unique identifier
 df$Identifier <- NA
 df$Identifier <- paste(df$ID, df$`Case Number`, sep = "-")
 
 ## Remove duplicate
-df <- df[order(df$Updated_On, decreasing = T), ]
+df <- df[order(df$`Updated On`, decreasing = T), ]
 df <- df[-which(duplicated(df$Identifier)), ]
 
 
@@ -128,92 +87,55 @@ df <- df[-which(duplicated(df$Identifier)), ]
 
 ## Get crime counts for each Primary Type 
 dfDistrict <- df %>%
-  group_by(Year_Month, District, Primary_Type) %>%
-  summarise(Num = n())
+  filter(!is.na(Datetime)) %>%
+  filter(!is.na(District)) %>%
+  filter(!is.na(`Primary Type`)) %>%
+  group_by(`Year Month`, District, `Primary Type`) %>%
+  summarise(Numerator = n())
 dfDistrict$ID <- NA
-dfDistrict$ID <- paste(dfDistrict$Year_Month, dfDistrict$District, sep = " ")
+dfDistrict$ID <- paste(dfDistrict$`Year Month`, dfDistrict$District, sep = " ")
 
 ## Get crime counts for each Month
 dfMonthly <- df %>%
-  group_by(Year_Month, District) %>%
-  summarise(Den = n())
+  filter(!is.na(Datetime)) %>% # to match Python
+  filter(!is.na(District)) %>%
+  group_by(`Year Month`, District) %>%
+  summarise(Denominator = n())
 dfMonthly$ID <- NA
-dfMonthly$ID <- paste(dfMonthly$Year_Month, dfMonthly$District, sep = " ")
-dfMonthly <- dfMonthly[ , c("ID", "Den")]
+dfMonthly$ID <- paste(dfMonthly$`Year Month`, dfMonthly$District, sep = " ")
+dfMonthly <- dfMonthly[ , c("ID", "Denominator")]
 
 ## Join
-dfDistrict <- left_join(dfDistrict, dfMonthly, by = "ID")
-dfDistrict$Crime_Rate <- NA
-dfDistrict$Crime_Rate <- dfDistrict$Num/dfDistrict$Den
+dfJoin <- left_join(dfDistrict, dfMonthly, by = "ID")
+dfJoin$`Crime Rate` <- NA
+dfJoin$`Crime Rate` <- dfJoin$Numerator/dfJoin$Denominator * 100
 
 
-#
-#x <- dfDistrict[dfDistrict$Primary_Type == "NARCOTICS", ]
-x <- as.data.frame(dfDistrict)
-x <- x[ , c("Year_Month", "District", "Primary_Type", "Crime_Rate")]
-x <- x[x$Primary_Type == "CRIM SEXUAL ASSAULT", ]
-#x <- x[!is.na(x$ID), ]
+#### Visualisation
 
-# District 1
-x <- x[x$District == 1, ]
-x <- x[ , c("Year_Month", "Primary_Type", "Crime_Rate")]
-
-#x$District <- as.factor(x$District)
-x$Primary_Type <- as.factor(x$Primary_Type)
+## Prepare data frame for Theft from 2016
+dfPrimaryType <- dfJoin[dfJoin$`Primary Type` == "THEFT", ]
+dfPrimaryType <- dfPrimaryType[dfPrimaryType$`Year Month` > "2015-12", ]
+#dfPrimaryType <- as.data.frame(dfPrimaryType)
+#dfPrimaryType$District <- as.factor(dfPrimaryType$District)
 
 
-ggraptR()
+### Plots
+
+## Crime frequency - theft
+p <- ggplot(dfPrimaryType, aes(`Year Month`, Numerator, group=District, color=District)) + geom_line() +
+  theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=0.5))
+print(p)
+
+## Crime rate - theft
+q <- ggplot(dfPrimaryType, aes(`Year Month`, `Crime Rate`, group=District, color=District)) + geom_line() + 
+  theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=0.5))
+print(q)
 
 
-p <- ggplot(x, aes(Year_Month, Crime_Rate)) + geom_point()
-p + facet_wrap(~Primary_Type)
-  
-  
-    
-
-
-#### Aggregate
-
-### Month
-
-## By Month and Primary_Type
-
-dfMonth_Primary_Type <- df %>%
-  group_by(Month, Month_Value, Primary_Type) %>%
-  summarise(Count = n())
-
-dfMonth_Primary_Type <- dfMonth_Primary_Type[!is.na(dfMonth_Primary_Type$Month), ]
-
-dfMonth_Primary_Type <- as.data.frame(dfMonth_Primary_Type)
-
-dfMonth_Primary_Type <- dfMonth_Primary_Type[order(dfMonth_Primary_Type$Month_Value), ]
-
-
-### By Hour and Primary_Type
-
-dfHour_Primary_Type <- df %>%
-  group_by(Hour, Primary_Type) %>%
-  summarise(Count = n())
-
-#dfHour_Primary_Type$Hour <- as.character(dfHour_Primary_Type$Hour)
-
-dfHour_Primary_Type <- dfHour_Primary_Type[!is.na(dfHour_Primary_Type$Hour), ]
-
-dfHour_Primary_Type <- as.data.frame(dfHour_Primary_Type)
-
-
-
-#### Visualise
-
-#df <- as.data.frame(df)
-ggraptR()
-
-
-
-
-
-#### Save data
+#### Save tidy data
 
 setwd("C:/Users/Muhsin Karim/Documents/Data/tidy_data/crimes_in_chicago")
-write.csv(df, file = "tidy.csv", row.names = F)
+write.csv(df, file = "tidy_R.csv", row.names = F)
 save(df, file = "tidy.Rdata")
+save(dfJoin, file = "join.Rdata")
--- data_wrangling_superdatascience.py
+++ data_wrangling_superdatascience.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+##### Crimes in Chicago - Data wrangling exmaple for SuperDataScience
 """
-
-##### Crimes in Chicago - Data wrangling exmaple
 
 #### Libraries
 
+## Load libraries
+import os
 import pandas as pd
 import numpy as np
-import os
+import datetime
+import matplotlib.pyplot as plt
 
 
-#### Get source data
+#### Get raw data
 
 ## Set working directory
 #! Change to your working directory here !#
-os.chdir('C:\\Users/Muhsin Karim\\Documents\\Data\\source_data\\kaggle\\crimes_in_chicago') # Section 5 Unit 1 Importing data into Python
-os.getcwd()
+os.chdir('C:\\Users/Muhsin Karim\\Documents\\Data\\raw_data\\kaggle\\crimes_in_chicago')
 
 ## Load in each file
 column_names = ['X1', 'ID', 'Case Number' ,'Date', 'Block', 
@@ -29,7 +27,6 @@ column_names = ['X1', 'ID', 'Case Number' ,'Date', 'Block',
 'Updated On', 'Latitude', 'Longitude', 'Location'] 
 
 df1 = pd.read_csv('Chicago_Crimes_2001_to_2004.csv', encoding='utf-8', names=column_names, skiprows=1)
-#df = df1
 df2 = pd.read_csv('Chicago_Crimes_2005_to_2007.csv', encoding='utf-8', names=column_names, skiprows=1)
 df3 = pd.read_csv('Chicago_Crimes_2008_to_2011.csv', encoding='utf-8', names=column_names, skiprows=1)
 df4 = pd.read_csv('Chicago_Crimes_2012_to_2017.csv', encoding='utf-8', names=column_names, skiprows=1)
@@ -44,26 +41,21 @@ del df1, df2, df3, df4
 ### Remove columns
 df = df.drop(['X1', 'IUCR', 'X Coordinate', 'Y Coordinate', 'Location'], axis=1)
 
-
-### Rename columns
+## Rename columns
 df = df.rename(columns={"Date": "Datetime"})
 
 
-## Change types
-#df['ID2'] = df['ID']
-df['ID'] = df['ID'].astype('str')
-#df['ID2'].apply(str)
+### Change data types
 
+df['ID'] = df['ID'].astype('str')
+
+## Remove decimals
 # http://stackoverflow.com/questions/17950374/converting-a-column-within-pandas-dataframe-from-int-to-string
 def trim_fraction(text):
     if '.0' in text:
         return text[:text.rfind('.0')]
     return text
-
 df.ID = df.ID.apply(trim_fraction)
-
-
-#pd.to_numeric(df['ID2'], errors='coerce')
 
 df['Case Number'] = df['Case Number'].astype(str)
 df['Datetime'] = df['Datetime'].astype(str)
@@ -83,44 +75,31 @@ df['Updated On'] = df['Updated On'].astype(str)
 df['Latitude'] = pd.to_numeric(df['Latitude'], errors='coerce')
 df['Longitude'] = pd.to_numeric(df['Longitude'], errors='coerce')
 
-## Remove duplicate rows
-#x = df.ix[(df['ID']==4786321)] # Good example for post!!!
 
-df = df.drop_duplicates()
+### Create datetimes
 
-
-### Change more data types
-
-## Datetime
-#df_copy = df
-
-#df['Datetime2'] = df['Datetime']
 df['Datetime'] = pd.to_datetime(df['Datetime'], format="%m/%d/%Y %I:%M:%S %p", errors='coerce') # Will not work without coerce... R does this automatically
-
-## Updated On
 df['Updated On'] = pd.to_datetime(df['Updated On'], format="%m/%d/%Y %I:%M:%S %p", errors='coerce')
 
 
 #### Create columns
 
 ### Create columns from datetime
-import datetime
 
 ## Month name
 df['Month'] = df['Datetime'].dt.strftime('%B')
 
 ## Month
-df['Month_Value'] = df['Datetime'].dt.month
+df['Month Value'] = df['Datetime'].dt.month
 
-## Year_Month
-df['Year_Month'] = df['Datetime'].dt.strftime('%Y-%m')
+## Year Month
+df['Year Month'] = df['Datetime'].dt.strftime('%Y-%m')
 
 ## Hour
 df['Hour'] = df['Datetime'].dt.hour
 
 
 #### Create unique identifier
-# Use ID and Case Number
 
 ## Create unique identifier
 df['Identifier'] = df['ID'] + '-' + df['Case Number']
@@ -129,12 +108,60 @@ df['Identifier'] = df['ID'] + '-' + df['Case Number']
 df = df.sort(['Updated On'], ascending=False)
 df = df.drop_duplicates('Identifier')
 
+
 #### Crime rate per District
 
 ## Get crime counts for each Primary Type 
+dfDistrict = df.groupby(['Year Month', 'District', 'Primary Type']).size().reset_index()
+dfDistrict = dfDistrict.rename(columns={0:'Numerator'})
+dfDistrict['ID'] = dfDistrict['Year Month'] + ' ' + dfDistrict['District'].astype(str)
+
+## Get crime counts for each Month
+dfMonth = df.groupby(['Year Month', 'District']).size().reset_index()
+dfMonth = dfMonth.rename(columns={0:'Denominator'})
+dfMonth['ID'] = dfMonth['Year Month'] + ' ' + dfMonth['District'].astype(str)
+dfMonth = dfMonth[['ID', 'Denominator']]
+
+## Join
+dfJoin = pd.merge(dfDistrict, dfMonth, how='left')
+dfJoin['Crime Rate'] = dfJoin['Numerator']/dfJoin['Denominator'] * 100
 
 
+#### Visualisation
+
+## Prepare data frame for Theft from 2016
+dfPrimaryType = dfJoin[(dfJoin['Primary Type'] == "THEFT")].reset_index()
+dfPrimaryType = dfPrimaryType[(dfPrimaryType['Year Month'] > "2015-12")]
 
 
+### Plots
+
+## Spread data frame
+
+## Crime frequency - theft
+dfCrimeFreq = dfPrimaryType.pivot(index='Year Month', columns='District')['Numerator']
+dfCrimeFreq = dfCrimeFreq.reset_index()
+x = list(range(dfCrimeFreq.shape[0]))
+y = dfCrimeFreq.drop(['Year Month'], axis=1)
+plt.plot(x, y)
+xticks = dfPrimaryType['Year Month']
+plt.xticks(x, xticks, rotation=45)
+plt.show()
+
+## Crime rate - theft
+dfCrimeRate = dfPrimaryType.pivot(index='Year Month', columns='District')['Crime Rate']
+dfCrimeRate = dfCrimeRate.reset_index()
+x = list(range(dfCrimeRate.shape[0]))
+y = dfCrimeRate.drop(['Year Month'], axis=1)
+plt.plot(x, y)
+xticks = dfPrimaryType['Year Month']
+plt.xticks(x, xticks, rotation=45)
+plt.show()
 
 
+#### Save tidy data
+
+os.chdir('C:\\Users\\Muhsin Karim\\Documents\\Data\\tidy_data\\crimes_in_chicago')   
+df.to_csv("tidy_py.csv", index=False)
+df.to_pickle("tidy.pkl") 
+df.to_pickle("join.pkl")
